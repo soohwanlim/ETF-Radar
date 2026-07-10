d560f96c500b6ae22aa82e7d078d3a4448bb353c
soohwanlim <swtee0506@kaist.ac.kr>
2026-07-10 10:36:33 +0900

    fix: remove initial shell before app mount

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root')
+rootElement.innerHTML = ''
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
